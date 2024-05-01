b5d98075bf5cf6d738bd28535ab334bd498724c8
pablo correia <pablocorreia2012@gmail.com>
2024-05-01 17:22:08 -0300

    feat: screen home and nav bar create and login methods

--- app/src/main/java/com/example/lealgym/ui/HomeFragment.kt
+++ app/src/main/java/com/example/lealgym/ui/HomeFragment.kt
@@ -5,14 +5,66 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.lealgym.R
+import com.example.lealgym.databinding.FragmentHomeBinding
+import com.example.lealgym.databinding.FragmentRecoverBinding
+import com.example.lealgym.ui.adapter.ViewPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class HomeFragment : Fragment() {
+    private lateinit var auth: FirebaseAuth
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        auth = Firebase.auth
+        configTablayout()
+        click()
+    }
+
+    private fun configTablayout() {
+        val adapter = ViewPagerAdapter(requireActivity())
+        binding.viewPage.adapter = adapter
+
+        adapter.addFragment(TodoFragment(), "A fazer")
+        adapter.addFragment(DoingFragment(), "Fazendo")
+        adapter.addFragment(DoneFragment(), "Feitas")
+
+        binding.viewPage.offscreenPageLimit = adapter.itemCount
+
+        TabLayoutMediator(
+            binding.tabs, binding.viewPage
+        ) { tab, position ->
+            tab.text = adapter.getTitle(position)
+        }.attach()
+    }
+
+    fun click() {
+        binding.btLogout.setOnClickListener {
+            logout()
+        }
+    }
+
+    fun logout() {
+        auth.signOut()
+        findNavController().navigate(R.id.action_homeFragment_to_navigation2)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/lealgym/ui/SplashFragment2.kt
+++ app/src/main/java/com/example/lealgym/ui/SplashFragment2.kt
@@ -10,10 +10,16 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.lealgym.R
 import com.example.lealgym.databinding.FragmentSplash2Binding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class SplashFragment2 : Fragment() {
     private var _binding: FragmentSplash2Binding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,7 +35,12 @@ class SplashFragment2 : Fragment() {
 
     private fun checkLogin() {
 //        manda o usuario para a tela de login após checar se esta logado
-        findNavController().navigate(R.id.action_splashFragment2_to_loginFragment2)
+        auth = Firebase.auth
+        if (auth.currentUser == null) {
+            findNavController().navigate(R.id.action_splashFragment2_to_navigation2)
+        } else {
+            findNavController().navigate(R.id.action_splashFragment2_to_homeFragment)
+        }
     }
 
     override fun onDestroy() {
